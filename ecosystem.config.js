2309834790e804c28f2c160f364584a29f99cf36
Vladimir Povalskij <lokireads@gmail.com>
2024-12-13 01:46:58 +0300

    Рефакторинг конфига

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp -Cr ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm ci && pwd && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
